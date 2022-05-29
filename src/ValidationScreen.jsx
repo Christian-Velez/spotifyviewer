476d0998a747effbba89db2e08b4d7af51be431d
Christian Velez Medina <christian.gerardo_@outlook.es>
2022-05-28 23:08:40 -0500

    loading spinner

--- src/ValidationScreen.jsx
+++ src/ValidationScreen.jsx
@@ -1,15 +1,22 @@
 import { setToken } from './store/spotifySlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {
+   useDispatch,
+   useSelector,
+} from 'react-redux';
+import {
+   useLocation,
+   useNavigate,
+} from 'react-router-dom';
 import React, { useEffect } from 'react';
 import StatsScreen from './StatsScreen';
-
+import { Spinner, VStack } from '@chakra-ui/react';
 
 const ValidationScreen = () => {
    const navigate = useNavigate();
    const dispatch = useDispatch();
    const search = useLocation().search;
-   const { setTokenSuccessfully, token } = useSelector((state) => state.spotify);
+   const { setTokenSuccessfully, token } =
+      useSelector((state) => state.spotify);
 
    const redirectToLogin = () => {
       navigate('/');
@@ -17,26 +24,35 @@ const ValidationScreen = () => {
 
    useEffect(() => {
       const startSettingToken = async () => {
-         const userCode = new URLSearchParams(search).get('code');
-         if (!userCode)
-            return redirectToLogin();
-         
+         const userCode = new URLSearchParams(
+            search
+         ).get('code');
+         if (!userCode) return redirectToLogin();
+
          dispatch(setToken(userCode));
       };
 
       startSettingToken();
    }, [dispatch]);
-      
+
    useEffect(() => {
-      if(!setTokenSuccessfully) {
+      if (!setTokenSuccessfully) {
          redirectToLogin();
       }
    }, [setTokenSuccessfully]);
 
-   return (
-      !token.access_token
-         ? <p>Loading...</p>
-         : <StatsScreen />
+   return !token.access_token ? (
+      <VStack w='full' paddingTop={40}>
+         <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='green.500'
+            size='xl'
+         />
+      </VStack>
+   ) : (
+      <StatsScreen />
    );
 };
 
